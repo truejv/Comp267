1e7e797ca29ef56b9c265cbd47b1656eb882adc6
Nyteisha Bookert <nyteishabookert@Nyteishas-MBP.triad.rr.com>
2020-11-09 14:28:00 -0500

    Added the three SQL files for Phase 4 of the Project

---
+++ TOPIC-data.sql
@@ -0,0 +1,4 @@
+-- This file will contain the SQL code to insert data into your database.
+-- At least 45 tuples among all tables
+-- Tables should have a minimum of 3 tuples
+
---
+++ TOPIC.sql
@@ -0,0 +1,5 @@
+-- – This file will contain the SQL code to create your database and tables. 
+
+-- DROP DATABASE IF EXISTS [CHANGEME];
+CREATE DATABASE [CHANGEME];
+USE [CHANGEME];
