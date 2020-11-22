515add2ffddba1cb3577d29478afeba1d33f2d37
truejv <jdvaughn@aggies.ncat.edu>
2020-11-22 16:31:30 -0500

    [updated]

--- MOVIETHEATER-queries.sql
+++ MOVIETHEATER-queries.sql
@@ -8,10 +8,10 @@ select * from customer;
 -- At least 5 queries that show how your database meets your use case/scenarios from Phase One and show the results. 
     -- Using SQL comments, describe your SQL query. 
     -- At least one basic query involving one table
-    -- At least one complex query involving two or more tables
+    -- At least one complex query involving two or more tables 
     -- At least one complex query involving aggregate functions and GROUP BY and HAVING SQL clauses
 
--- finds total sales for 8-15-2020
+-- finds total sales for 8-15-2020 
 select SALE_DATE, sum(TICKET_PRICE) from ticket where SALE_DATE = '2020-08-15';
 
 
